3e86bf80f908434585145418607aefdf65c350d9
Adam Rumpf <arumpf@floridapoly.edu>
2023-04-17 16:41:52 -0400

    Minor edits.

--- gradient_descent/main.m
+++ gradient_descent/main.m
@@ -2,8 +2,9 @@
 %
 % This is the main driver for a collection of script files that should be
 % placed in the same directory, including:
-%	showpath.m
-%	graddescent.m
+%   showgrad.m
+%   showpath.m
+%   graddescent.m
 
 f1 = @(x) 0.125*x'*[2, 1; 1, 3]*x - [2, -1]*x;
 fp1 = @(x) 0.25*[2, 1; 1, 3]*x - [2; -1];
@@ -20,5 +21,5 @@ fp2 = @(x) [2, 1; 1, 20]*x - [5; 3];
 exact2 = [97/39; 1/39];
 
 %sols2 = graddescent(@(x) f2(x), @(x) fp2(x), [-3; -1], 10, 1.0);
-%showpath(@(x) f2(x), sols2, [-4, 4], [-2, 2], "Gradient Descent, theta = 1.0", exact2);
+%showpath(@(x) f2(x), sols2, [-4, 4], [-2, 2], "Gradient Descent, gamma = 1.0", exact2);
 %showgrad(@(x) f2(x), @(x) fp2(x), sols2);
